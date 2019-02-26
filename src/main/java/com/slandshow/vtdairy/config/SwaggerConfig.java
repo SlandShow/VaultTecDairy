9a074f6664956467d575b3a1713f8c628640dd5d
SlandShow <SlandShow12345@mail.ru>
2019-02-27 00:02:10 +0300

    Update Swagger config for JWT token-based auth in swagger-ui for secured endpoints

--- src/main/java/com/slandshow/vtdairy/config/SwaggerConfig.java
+++ src/main/java/com/slandshow/vtdairy/config/SwaggerConfig.java
@@ -1,29 +1,61 @@
 package com.slandshow.vtdairy.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.List;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket swaggerSpringfoxDocket() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
-                .select()
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()));
+
+        docket = docket.select()
                 .apis(RequestHandlerSelectors.basePackage("com.slandshow.vtdairy.controller"))
                 .paths(PathSelectors.any())
                 .build();
+
+        return docket;
     }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/entry/criteria"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
 
     /* Describe APIs */
     private ApiInfo apiInfo() {
